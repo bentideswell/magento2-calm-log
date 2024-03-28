79786ec4b850e563e8215519457d042f1c597f5c
Ben Tideswell <ben@fishpig.com>
2024-03-28 16:23:04 +0000

    1.0.0

---
+++ App/Logger/SilencerProxy.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ *
+ */
+namespace FishPig\CalmLog\App\Logger;
+
+class SilencerProxy extends \Magento\Framework\Logger\LoggerProxy
+{
+    /**
+     *
+     */
+    public function info($message, array $context = [])
+    {
+        if (!$this->isIgnorableMessage($message)) {
+            parent::info($message, $context);
+        }
+    }
+
+    /**
+     *
+     */
+    private function isIgnorableMessage($message): bool
+    {
+        if (!is_string($message)) {
+            return false;
+        }
+
+        if (strpos($message, 'Broken reference:') === 0) {
+            return true;
+        } elseif (strpos($message, 'Add of item with id') !== false) {
+            return true;
+        } elseif (strpos($message, ' was removed') !== false) {
+            return true;
+        }
+
+        return false;
+    }
+}
